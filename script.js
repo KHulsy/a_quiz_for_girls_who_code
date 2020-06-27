4306705ea787f43527ff15185fb4278aa87a25c7
KHulsy <66654951+KHulsy@users.noreply.github.com>
2020-06-27 15:50:31 -0700

    Script.js
    Incorporated code for quiz. It doesn't work. I went to tutoring. I did all the things. Ty.

---
+++ script.js
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<body>
+<p id="demo"></p>
+<script>
+
+var questions =[1, 2, 3]              
+questions[0]
+var question= {question}: "What percentage of high school girls wanted to major in computer science in 2019?";
+choices: ["javascript quiz"],["all the ladies"],["c is for cookie"],["less than one percent"]
+answer: "less than one percent" ;
+var questions=[1, 2, 3] ;
+questions[1];
+var question= {question}: "What do the letters NaN stand for?";
+choices: ["a super sweet nine inch nails cover band"],["Nickelodeon Atom Nitty"],[Not a Number];
+answer: "Not a number" ;
+var questions=[1, 2, 3] ;
+questions[2];
+var question= {question}: "What is vanilla Javascript?" ;
+choices: ["It's my faavorite frappucino and I LOVE FALL"],["a super awesome screenwriting app"],["Javascript without the JQuery"];
+answer: "Javascript without the JQuery" ;
+</script>
+    body >
